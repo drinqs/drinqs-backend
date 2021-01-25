5d5c4cd92fd61a78a0c5fc6ead86557fbc4aef86
Fabian Ingenhorst <f_inge03@uni-muenster.de>
2021-01-25 20:56:38 +0100

    adds commands to get categories from wikipedia
    exec getCategoriesCSV only when changes to regex were made
    scrapeTags gets all categories to all ingredients and takes >1h

---
+++ drinqsapp/management/commands/getCategoriesAsCSV.py
@@ -0,0 +1,57 @@
+from abc import ABC
+import requests
+from django.core.management.base import BaseCommand
+from drinqsapp.models import Ingredient, IngredientTag
+from re import search
+import numpy as np
+import pandas as pd
+
+
+def createWikiCategoryTree(category, result, regexFilter):
+    url = 'https://en.wikipedia.org/w/api.php'
+
+    params = dict(
+        format='json',
+        action='query',
+        list='categorymembers',
+        cmtitle=category,
+        cmlimit='max',
+        cmtype='subcat'
+    )
+
+    resp = requests.get(url=url, params=params)
+    data = resp.json()
+
+    for category in data["query"]["categorymembers"]:
+        if not search(regexFilter, category['title']) and category['title'] not in result:
+            print(category['title'])
+            result.append(category['title'])
+            result = createWikiCategoryTree(category=category['title'], result=result, regexFilter=regexFilter)
+
+    return result
+
+class Command(BaseCommand, ABC):
+
+    def handle(self, *args, **kwargs):
+
+        drinksRegexFilter = "[C|c]ompanies|[B|b]rewer[i|y]|[P|p]ubs|continent|country|region|brewers|" \
+                    "industry|culture|organizations|[P|p]eople|logos|[W|w]ineries|industr|establishment|" \
+                    "[H|h]ot springs|[G|g]eographical|science|[H|h]istory"
+
+        foodRegexFilter = "[C|c]ompanies|[B|b]rewer[i|y]|[P|p]ubs|continent|country|region|brewers|" \
+                        "industry|culture|organizations|[P|p]eople|logos|[W|w]ineries|industr|establishment|" \
+                        "[H|h]ot springs|[G|g]eographical|science|[H|h]istory|product brands|prepared|technique|by type|Lists|" \
+                        "Condiments|Cuisine|Dips|excrement|combinations|logos|Meat|Pizza|Wedding food|Water|" \
+                        "McDonald|plantation|Oils|pastries|[C|c]ake|Cocktails"
+
+        foodcategories = createWikiCategoryTree(category='Category:Foods', result=[]
+                                            ,regexFilter=foodRegexFilter)
+        foods = np.asarray(foodcategories)
+        pd.DataFrame(foods).to_csv("data/foodCategories.csv")
+
+        drinkcategories = createWikiCategoryTree(category='Category:Drinks', result=[]
+                                            ,regexFilter=drinksRegexFilter)
+        drinks = np.asarray(drinkcategories)
+        pd.DataFrame(drinks).to_csv("data/drinkCategories.csv")
+
+
---
+++ drinqsapp/management/commands/scrapeTags.py
@@ -0,0 +1,175 @@
+from abc import ABC
+import requests
+from django.core.management.base import BaseCommand
+from drinqsapp.models import Ingredient, IngredientTag
+from re import search
+import numpy as np
+import pandas as pd
+
+
+def getAllTags(self):
+
+    drinkcategoriesCSV = pd.read_csv("data/drinkCategories.csv")
+    drinkcategoriesList = drinkcategoriesCSV["0"]
+
+    foodcategoriesCSV = pd.read_csv("data/foodCategories.csv")
+    foodcategoriesList = foodcategoriesCSV["0"]
+
+    for ingredient in Ingredient.objects.all():
+        url = 'https://en.wikipedia.org/w/api.php'
+
+        print('Try to search for ingredient...')
+        params = dict(
+            format='json',
+            action='opensearch',
+            search=ingredient.name,
+            limit=100
+        )
+
+        resp = requests.get(url=url, params=params)
+        data = resp.json()
+        print(data[1])
+        if data[1]:
+            print('iterate over search result...')
+            for searchResult in data[1]:
+                print('try... ' + searchResult)
+                params = dict(
+                    format='json',
+                    action='query',
+                    prop='categories',
+                    cllimit='max',
+                    titles=searchResult,
+                    redirects='true',
+                )
+
+                resp = requests.get(url=url, params=params)
+                data = resp.json()
+                for page in data["query"]["pages"]:
+                    print(ingredient)
+                    if page != "-1":
+                        for category in data["query"]["pages"][page]["categories"]:
+                            print(category["title"])
+                            if category["title"] in drinkcategoriesList.values \
+                                or category["title"] in foodcategoriesList.values:
+                                print("is in list!")
+                                c = category["title"].replace("Category:","")
+                                if len(c) < 127:
+                                    ingredient.ingredient_tags.add(IngredientTag.objects.get_or_create(name=c)[0])
+                                    print(c + " --- saved into DB")
+
+        else:
+            splitString = ingredient.name.split()
+            print('Try to search for SPLIT ingredient...' + str(splitString))
+            if len(splitString) > 2:
+                tempString = splitString.copy()
+                del tempString[0]
+                withoutFirst = ' '.join(tempString)
+                tempString = splitString.copy()
+                del tempString[-1]
+                withoutLast = ' '.join(tempString)
+                splitString.append(withoutFirst)
+                splitString.append(withoutLast)
+
+            for split in splitString:
+                print('split:' + split)
+                params = dict(
+                    format='json',
+                    action='opensearch',
+                    search=split,
+                    limit=100
+                )
+
+                resp = requests.get(url=url, params=params)
+                data = resp.json()
+                print(data[1])
+                print('iterate over splitted search result...')
+                if data[1]:
+                    for searchResult in data[1]:
+                        print('try... ' + searchResult)
+                        params = dict(
+                            format='json',
+                            action='query',
+                            prop='categories',
+                            cllimit='max',
+                            titles=searchResult,
+                            redirects='true',
+                        )
+                        resp = requests.get(url=url, params=params)
+                        data = resp.json()
+                        for page in data["query"]["pages"]:
+                            print(ingredient)
+                            if page != "-1":
+                                for category in data["query"]["pages"][page]["categories"]:
+                                    print(category["title"])
+                                    if category["title"] in drinkcategoriesList.values or category[
+                                        "title"] in foodcategoriesList.values:
+                                        print("is in list!")
+                                        c = category["title"].replace("Category:", "")
+                                        if len(c) < 127:
+                                            ingredient.ingredient_tags.add(
+                                                IngredientTag.objects.get_or_create(name=c)[0])
+                                            print(c + " --- saved into DB")
+
+    for ingredient in Ingredient.objects.filter(ingredient_tags__isnull=True):
+        url = 'https://en.wikipedia.org/w/api.php'
+        splitString = ingredient.name.split()
+        print('Try to search for SPLIT ingredient...' + str(splitString))
+        if len(splitString) > 2:
+            tempString = splitString.copy()
+            del tempString[0]
+            withoutFirst = ' '.join(tempString)
+            tempString = splitString.copy()
+            del tempString[-1]
+            withoutLast = ' '.join(tempString)
+            splitString.append(withoutFirst)
+            splitString.append(withoutLast)
+
+        for split in splitString:
+            print('split:' + split)
+            params = dict(
+                format='json',
+                action='opensearch',
+                search=split,
+                limit=100
+            )
+
+            resp = requests.get(url=url, params=params)
+            data = resp.json()
+            print(data[1])
+            print('iterate over splitted search result...')
+            if data[1]:
+                for searchResult in data[1]:
+                    print('try... ' + searchResult)
+                    params = dict(
+                        format='json',
+                        action='query',
+                        prop='categories',
+                        cllimit='max',
+                        titles=searchResult,
+                        redirects='true',
+                    )
+                    resp = requests.get(url=url, params=params)
+                    data = resp.json()
+                    for page in data["query"]["pages"]:
+                        print(ingredient)
+                        if page != "-1":
+                            for category in data["query"]["pages"][page]["categories"]:
+                                print(category["title"])
+                                if category["title"] in drinkcategoriesList.values or category[
+                                    "title"] in foodcategoriesList.values:
+                                    print("is in list!")
+                                    c = category["title"].replace("Category:", "")
+                                    if len(c) < 127:
+                                        ingredient.ingredient_tags.add(
+                                            IngredientTag.objects.get_or_create(name=c)[0])
+                                        print(c + " --- saved into DB")
+
+
+class Command(BaseCommand, ABC):
+
+    def handle(self, *args, **kwargs):
+        getAllTags(self)
+
+
+
+
